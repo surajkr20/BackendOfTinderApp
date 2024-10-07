b67f426932132d7994b40bb3757181b9769e7751
surajkr20 <suraj71308kumar@gmail.com>
2024-10-07 16:28:02 +0530

    creating server and some routes

---
+++ src/app.js
@@ -0,0 +1,29 @@
+
+// Import the express
+const express = require('express');
+
+// Create an instance of express
+const app = express();
+
+// First route: responds to requests made to the root URL "/"
+app.get("/", (req, res) => {
+    res.send("hello from the server");
+});
+
+// Second route: responds to requests made to "/hello"
+app.get("/hello", (req, res) => {
+    res.send("hello hello hello.....");
+});
+
+app.get("/test", (req, res) => {
+    res.send("hello test.....");
+});
+
+app.get("/contact", (req, res) => {
+    res.send("hello contact me.....");
+});
+
+// Start the server and listen on port 3000
+app.listen(3000, () => {
+    console.log("server started on port no 3000");
+});
