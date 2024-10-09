78f2522cc3c12834ae99e4fd1f22e3a461f813a4
surajkr20 <suraj71308kumar@gmail.com>
2024-10-09 07:27:50 +0530

    exploring advance routes consepts

--- src/app.js
+++ src/app.js
@@ -5,42 +5,46 @@ const express = require('express');
 // Create an instance of express
 const app = express();
 
-// First route: responds to requests made to the root URL "/"
-app.get("/", (req, res) => {
-    res.send("hello from the server");
-});
+// routing using (?, +, (), *) 
 
-// Second route: responds to requests made to "/hello"
-app.get("/hello", (req, res) => {
-    res.send("hello hello hello.....");
-});
-
-app.get("/test", (req, res) => {
-    res.send("hello test.....");
-});
-
-app.get("/contact", (req, res) => {
-    res.send("hello contact me.....");
-});
-
-// creating multiples routes
-app.use('/user',(req, res) =>{
-    res.send('i am from user')
+app.get('/about', (req, res) =>{
+    res.send("this is your about data")
 })
 
+// using of question mark(? previes letter is optional)
+app.get('/ab?c',(req, res) =>{
+    res.send("connection successful")
+})
+// + means you increase the no of b multiple times but response is send same
+app.get('/ab+c',(req, res) =>{
+    res.send("connection successful 2")
+})
+
+// * means use can any letters, words sentences between ab and c, it will be perfectly work
+app.get('/ab*c',(req, res) =>{
+    res.send("connection multiple times 3")
+})
+
+// you can use any word etc before fly and end with fly
+app.get(/.*fly$/,(req, res) =>{
+    res.send("connection multiple times 4")
+})
+
+// means you can any word, latter sentence between //, and you see the result is same.
+app.get(/a/,(req, res) =>{
+    res.send("connection multiple times 5")
+})
+
+// data from url
 app.get('/user',(req, res) =>{
-    res.send({firsname : 'suraj', lastName : 'kumar'})
+    console.log(req.query)
+    res.send("it's a user data")
 })
 
-app.post('/user',(req, res) =>{
-    console.log({firsname : 'suraj', lastName : 'kumar'})
-    res.send("user data")
+app.get('/user/:707',(req, res) =>{
+    console.log(req.params)
+    res.send("another data")
 })
-
-app.delete('/user',(req, res) =>{
-    res.send("deleted successfully")
-})
-
 
 // Start the server and listen on port 3000
 app.listen(3000, () => {
