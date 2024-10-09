5e3df268e629fcd08ced09a95bc6d3bf02df75a2
surajkr20 <suraj71308kumar@gmail.com>
2024-10-09 15:53:47 +0530

    middleware in depth

--- src/app.js
+++ src/app.js
@@ -1,32 +1,44 @@
 // Import the express
 const express = require("express");
+const {AdminAuth, UserAuth} = require('./middlewares/Auth')
 
 // Create an instance of express
 const app = express();
 
-app.use(
-  "/user",
-  (req, res, next) => {
-    res.json('now you are on the first route');
-    next();
-  }
-);
+app.use('/admin',AdminAuth);
+app.use('/admin',UserAuth);
 
-app.use(
-  "/user",
-  (req, res, next) => {
-    res.json("hello suraj !");
-    next();
-  }
-);
+// Route that will be accessed if user is authorized
+app.get('/user', UserAuth, (req, res) => {
+    res.send('welcome user brother you are already an authorized person');
+});
 
-app.use(
-  "/user",
-  (req, res, next) => {
-    res.json("hello Abhinav how are you");
-    // next();
-  }
-);
+app.post('/user/login', AdminAuth, (req, res) =>{
+    res.send('congratulations your are successfully loged in')
+})
+
+app.get('/admin/allData',(req, res) =>{
+    // logic of checking if the request is authorized
+    const token = 'xyz';
+    const adminAuthorized = token === 'xyz';
+    if(adminAuthorized){
+        res.json('welcome Abhinav, you are already authorized person');
+    }else{
+        res.status(404).send("sorry you are an unauthorized person! please get back")
+    }
+    res.send('acepted you inmitation')
+})
+
+app.get('/user/admin/delete',(req, res) =>{
+    // logic of checking if the request is authorized
+    const token = 'xyzz';
+    const adminAuthorized = token === 'xyz';
+    if(adminAuthorized){
+        res.json('you unauthorized person is deleted');
+    }else{
+        res.status(404).send("sorry we can't proceed")
+    }
+})
 
 // Start the server and listen on port 3000
 app.listen(3000, () => {
