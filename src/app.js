e21d129627bdda6382b929e772ba06c541a08f58
surajkr20 <suraj71308kumar@gmail.com>
2024-10-09 13:29:25 +0530

    middleware-chain

--- src/app.js
+++ src/app.js
@@ -4,20 +4,27 @@ const express = require("express");
 // Create an instance of express
 const app = express();
 
-// handling multiple response
 app.use(
   "/user",
   (req, res, next) => {
-    // res.json({ firstName: "Abhinav", lastName: "bhardwaaj" });
+    res.json('now you are on the first route');
     next();
-  },
+  }
+);
+
+app.use(
+  "/user",
   (req, res, next) => {
-    // res.json("respone 2!");
+    res.json("hello suraj !");
     next();
-  },
+  }
+);
+
+app.use(
+  "/user",
   (req, res, next) => {
-    res.json("respone 3rd");
-    next();                         //it's expexting one routes more handler
+    res.json("hello Abhinav how are you");
+    // next();
   }
 );
 
