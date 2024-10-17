fefc2764d4c07ddeb64a43d85a464cf8050e7ce5
surajkr20 <suraj71308kumar@gmail.com>
2024-10-17 14:20:00 +0530

    A dynamic signup api, using express.json middleware

--- src/app.js
+++ src/app.js
@@ -10,17 +10,24 @@ const app = express();
 // aquiring user.js file in app.js
 const User = require('./models/user');
 
+app.use(express.json());
+
 app.post('/signup', async(req, res) =>{
+
+  // console.log(req.body)
+
   // creating a new instance of the user model
 
-  const user = new User({
-    firstName : 'Abhinav',
-    lastName : 'bhardwaj',
-    emailId: 'abhinavsuraj@gmail.com',
-    password : 'Abhinav@7860',
-    age : 20,
-    gender : 'male'
-  })
+  const user = new User(req.body);
+
+  // const user = new User({
+  //   firstName : 'Abhinav',
+  //   lastName : 'bhardwaj',
+  //   emailId: 'abhinavsuraj@gmail.com',
+  //   password : 'Abhinav@7860',
+  //   age : 20,
+  //   gender : 'male'
+  // })
 
   try {
     await user.save();
@@ -29,7 +36,6 @@ app.post('/signup', async(req, res) =>{
     res.status(400).send('error saving user')
   }
 })
-
 
 // Start the server and listen on port 3000
 app.listen(3000, () => {
