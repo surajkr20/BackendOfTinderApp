6bf3c797f9ba4abea201ab685074523c6603010a
surajkr20 <suraj71308kumar@gmail.com>
2024-10-17 15:16:29 +0530

    getting users emaiId

--- src/app.js
+++ src/app.js
@@ -12,22 +12,26 @@ const User = require('./models/user');
 
 app.use(express.json());
 
+// get user by emailId
+app.get('/user', async(req, res) =>{
+  const userEmail = req.body.emailId;
+
+  try{
+    const user = await User.find({emailId : userEmail})
+    if(user.length === 0){
+      res.status(404).send('user not found');
+    }else{
+      res.send(user);
+    }
+  }catch(error){
+    res.send('something went wrong')
+  }
+})
+
 app.post('/signup', async(req, res) =>{
 
-  // console.log(req.body)
-
   // creating a new instance of the user model
-
   const user = new User(req.body);
-
-  // const user = new User({
-  //   firstName : 'Abhinav',
-  //   lastName : 'bhardwaj',
-  //   emailId: 'abhinavsuraj@gmail.com',
-  //   password : 'Abhinav@7860',
-  //   age : 20,
-  //   gender : 'male'
-  // })
 
   try {
     await user.save();
